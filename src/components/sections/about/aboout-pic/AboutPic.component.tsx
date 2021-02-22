c77ca930de930036ad3ba6b83be36b7e4b043a79
Mashima Button <zimashima@gmail.com>
2021-02-21 19:13:03 -0500

    changed alt in AboutPic

--- src/components/sections/about/aboout-pic/AboutPic.component.tsx
+++ src/components/sections/about/aboout-pic/AboutPic.component.tsx
@@ -4,7 +4,7 @@ export const AboutPic: React.FC<{}> = () => {
 
     return (
         <div className="about-pic m-2 shadow-lg bg-body rounded overflow-hidden" id="aboutPic">
-           <img src={ mbPic } alt="Mashima Button's Picture" />
+           <img src={ mbPic } alt="Mashima Button" />
         </div>
     )
 };

61109c274a73945b608a361327eab0630c61ed71
Mashima Button <zimashima@gmail.com>
2021-02-22 13:16:44 -0500

    updated

--- src/components/layout/navigation/Navigation.component.tsx
+++ src/components/layout/navigation/Navigation.component.tsx
@@ -8,7 +8,7 @@ export const Navigation = () => {
             <Navbar.Brand href="#home" className="d-flex align-items-center">
                 <div className="logo-div">
                 <Image imgCode="mblogo" altText="MB Logo" /></div>
-                <h1 className="text-white p-3">
+                <h1 className="text-white pl-3">
                 Mashima Button</h1>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navigation-menu" />
